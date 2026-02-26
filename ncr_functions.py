e5dbf9b9b241505484cc31a7969e7030aad3b647
James Young <james@jmsmetaltec.com>
2026-02-26 17:10:16 +0000

    ncr log work

--- ncr_functions.py
+++ ncr_functions.py
@@ -4,8 +4,37 @@ import pandas as pd
 
 def clean_ncr_data(df):
     # clean data massively
-    # filter nulls
-    df = df[df['Description'].isna() == False]
+
+    # parse dates
+    df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
+    df["Date Complete"] = pd.to_datetime(df["Date Complete"], errors="coerce")
+
+    # Standardise Y/N columns
+    yn_cols = ["Returned to Customer", "Report Done"]
+    for col in yn_cols:
+        if col in df.columns:
+            df[col] = (
+                df[col]
+                .astype(str)
+                .str.strip()
+                .str.upper()
+                .replace({"Y": "Yes", "N": "No"})
+            )
+
+    # completion flag for suggeastive action implemented
+    df["Suggested Corrective Action Implemented"] = df["Date Complete"].notna().map(
+        {True: "Yes", False: "No"}
+    )
+    # fill missing data with 'Data Unknown' for str columns
+    for col in ['Non Conformance Received/Recorded By', 'Description', 'Department', 'Root Cause']:
+        df[col] = df[col].fillna("Data Unknown")
+
+    # tidy input (strip trailing spaces and standardise capitilasition)
+    cols_to_tidy = ["Customer", "Department"]
+    for col in cols_to_tidy:
+        df[col] = (
+            df[col].astype(str).str.strip().str.replace(r"\s+", " ", regex=True).str.title()
+        )
     return df
 
 def render_basic_counts(df):
@@ -47,6 +76,20 @@ def render_basic_counts(df):
     # number that are internal
     # number that are external
     # number per standardised root cause
+    root_causes = after_2025["Root Cause"].dropna().unique().tolist()
+    st.markdown(f"Root Causes List: {root_causes}")
+    root_causes_df = (
+        after_2025["Root Cause"]
+        .value_counts()
+        .reset_index()
+    )
+
+    root_causes_df.columns = ["Root Cause", "count"]
+    root_causes_df = root_causes_df[root_causes_df["count"] >= 2]
+    root_causes_df["percentage of all"] = (
+        (root_causes_df["count"] / len(after_2025.index)) *100
+    )
+    st.dataframe(root_causes_df)
     # number per suggested corrective action completed
     # number returned to customer
     # number of reports done
