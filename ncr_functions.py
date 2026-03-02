e10169fe8b1ce9ff8361bf7a17bdf90bcef1c6a4
James Young <james@jmsmetaltec.com>
2026-03-02 13:07:40 +0000

    ncr log page visualised

--- ncr_functions.py
+++ ncr_functions.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
+from data import *
 
 def clean_ncr_data(df):
     # clean data massively
@@ -35,28 +36,49 @@ def clean_ncr_data(df):
         df[col] = (
             df[col].astype(str).str.strip().str.replace(r"\s+", " ", regex=True).str.title()
         )
+
+    # log as internal (replacing null), "No Official NCR" (replacing n/a) or the customer NCR for "Customer NCR No column"
+    df["Customer NCR No."] = (
+        df["Customer NCR No."].replace("N/A", "No Official NCR No.").fillna("Internal")
+    )
+
+    # sort companies being reported differently
+    df = apply_company_grouping(df)
     return df
 
-def render_basic_counts(df, date_filter):
+def render_debug_data(df, date_filter):
+    st.markdown("### DEBUGGING ZONE")
     # output raw counts of numbers
     # total number of ncrs
     date_filter = pd.to_datetime(date_filter)
-    after_2025 = df[df["Date"] >= date_filter]
-    st.markdown(f"NCRs from {date_filter}: {len(after_2025.index)}")
+    date_filtered_df = df[df["Date"] >= date_filter]
+    st.dataframe(date_filtered_df)
+    st.markdown(f"NCRs from {date_filter}: {len(date_filtered_df.index)}")
 
     # number by department
-    list_and_df("Department", after_2025)
+    list_and_df("Department", date_filtered_df)
     # number by customer
-    list_and_df("Customer", after_2025)
+    list_and_df("Customer", date_filtered_df)
     # number by person recording ncr
-    list_and_df("Non Conformance Received/Recorded By", after_2025)
+    list_and_df("Non Conformance Received/Recorded By", date_filtered_df)
     # number that are internal
+    st.markdown(f"No. of Internal NCRS: {(date_filtered_df["Customer NCR No."] == "Internal").sum()}")
     # number that are external
+    st.markdown(f"No. of External NCRS: {(date_filtered_df["Customer NCR No."] != "Internal").sum()}")
     # number per standardised root cause
-    list_and_df("Root Cause", after_2025)
+    list_and_df("Root Cause", date_filtered_df)
     # number per suggested corrective action completed
+    SCA_completed = len(date_filtered_df) - (date_filtered_df["Suggested Corrective Action Implemented"] == "Yes").sum()
+    SCA_percent = round((SCA_completed/len(date_filtered_df))*100,2)
+    st.markdown(f"Number of NCRS with suggested corrective action completed: {SCA_completed} ({SCA_percent}%)")
     # number returned to customer
+    RTC_completed = len(date_filtered_df) - (date_filtered_df["Returned to Customer"]=="Yes").sum()
+    RTC_percent = round((RTC_completed/len(date_filtered_df))*100,2)
+    st.markdown(f"Number of NCRS returned to customer: {RTC_completed} ({RTC_percent}%)")
     # number of reports done
+    report_done = len(date_filtered_df) - (date_filtered_df["Report Done"]=="Yes").sum()
+    report_percent = round((report_done/len(date_filtered_df))*100,2)
+    st.markdown(f"Number of NCRS with report completed: {report_done} ({report_percent}%)")
 
 def list_and_df(column, df):
     unique_list = df[column].dropna().unique().tolist()
@@ -65,7 +87,85 @@ def list_and_df(column, df):
         df[column].value_counts().reset_index()
     )
     unique_df.columns = [column, "count"]
-    unique_df["percentage of all"] = (
+    unique_df[f"% of total"] = (
         (unique_df["count"] / len(df.index)) *100
     )
-    st.dataframe(unique_df)    
+    st.dataframe(unique_df) 
+
+def render_df(df, col, header):
+    col.markdown(f"Count By {header}")
+    unique_df = (
+        df[header].value_counts().reset_index()
+    )
+    unique_df.columns = [header, "count"]
+    unique_df[f"% of total"] = (
+        round(((unique_df["count"] / len(df.index)) *100), 2)
+    )
+    col.dataframe(unique_df, hide_index=True) 
+
+def render_progress_bars(df, col):
+    # Suggested Corrective Action
+    SCA_completed = len(df) - (df["Suggested Corrective Action Implemented"] == "Yes").sum()
+    SCA_percent = round((SCA_completed/len(df)),2)
+    col.progress(float(SCA_percent), f"Number of NCRS with suggested corrective action completed: {SCA_completed} ({SCA_percent*100}%)")
+    # Returned to Customer
+    RTC_completed = len(df) - (df["Returned to Customer"] == "Yes").sum()
+    RTC_percent = round((RTC_completed/len(df)),2)
+    col.progress(float(RTC_percent), f"Number of NCRS returned to customer: {RTC_completed} ({RTC_percent*100}%)")
+    # Report Done
+    report_done = len(df) - (df["Report Done"] == "Yes").sum()
+    report_percent = round((report_done/len(df)),2)
+    col.progress(float(report_percent), f"Number of NCRS with report completed: {int(report_done)} ({report_percent*100:.1f}%)")
+
+def render_internal_chart(df, col):
+    counts = df["Customer NCR No."].value_counts()
+
+    internal = counts.get("Internal", 0)
+    external = counts.sum() - internal
+
+    col.metric("Internal NCRs", internal)
+    col.metric("External NCRs", external)
+
+    pie_data = pd.DataFrame({
+        "Type": ["Internal", "External"],
+        "Count": [internal, external]
+    })
+
+
+    # Pie chart for proportion
+    fig = px.pie(
+        pie_data, names="Type", values="Count", hole=0.4, color="Type",
+        color_discrete_map={"Internal": "#2ecc71", "External": "#3498db"}
+    )
+    col.plotly_chart(fig, use_container_width=False)
+
+def render_sales_order_chart(df, date_filter, col):
+    # read in all sales orders since date that is filtered from statii
+    # done reading from a hardcoded CSV file, will upgrade to be an API call when we get that functionality from statii
+    # have to do a dump out and save to NCR Log/ALL SALES ORDERS.csv on sharepoint
+    so_df = load_so_sp()
+    # filter so_df by date filter
+    date_filter = pd.to_datetime(date_filter)
+    so_df["Date Created"] = pd.to_datetime(so_df["Date Created"])
+    so_df = so_df[so_df["Date Created"] >= date_filter]
+    # st.dataframe(so_df) # debug
+
+    num_so = len(so_df)
+    num_ncr = len(df)
+
+    # visualise data
+    col.metric(f"Number of NCRs since {date_filter}:", num_ncr)
+    col.metric(f"Number of SOs since {date_filter}:", num_so)
+
+    pie_data = pd.DataFrame({
+        "Type": ["NCRs", "SOs"],
+        "Count": [num_ncr, num_so]
+    })
+
+
+    # Pie chart for proportion
+    fig = px.pie(
+        pie_data, names="Type", values="Count", hole=0.4, color="Type",
+        color_discrete_map={"NCRs": "#2ecc71", "SOs": "#3498db"}
+    )
+    col.plotly_chart(fig, use_container_width=False)
--- pages/4_NCR_Log.py
+++ pages/4_NCR_Log.py
@@ -20,10 +20,31 @@ render_logo(tcol1)
 df = load_data_ncr_sp()
 df = clean_ncr_data(df)
 
-st.dataframe(df)
-date_filter = st.date_input("Showing NCRS after this date: ", value=datetime.datetime(2025,1,1))
-render_basic_counts(df, date_filter)
+# render overview dataframes
+col1, col2 = st.columns([1, 4])
+with col1:
+    date_filter = st.date_input("Showing NCRS after this date: ", value=datetime.datetime(2025,1,1))
+date_filter = pd.to_datetime(date_filter)
+df = df[df["Date"] >= date_filter]
 
+col1, col2, col3, col4 = st.columns(4)
+render_df(df, col1, "Customer Grouped")
+render_df(df, col2, "Department")
+render_df(df, col3, "Non Conformance Received/Recorded By")
+render_df(df, col4, "Root Cause")
+
+# render charts and progress bars
+col1, col2, col3 = st.columns(3)
+render_internal_chart(df, col2)
+render_sales_order_chart(df, date_filter, col1)
+render_progress_bars(df, col3)
+
+# ts debugs
+debug = st.toggle("View Debug Data?", value=False)
+if debug:
+    render_debug_data(df, date_filter)
+
+# NOTES
 # recorded by
 
 # Customer NCR nos - n/a means none, blank means internal
@@ -32,7 +53,7 @@ render_basic_counts(df, date_filter)
 
 # standardised by description
 
-# add columns for quantity, part numner, Assembly as well? - ask eric
+# add columns for quantity, part number, Assembly as well? - ask eric
 
 # multiple ways to be complete
 # either by sending it back or by the corrective action being implemented
