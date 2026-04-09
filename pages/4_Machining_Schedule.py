6e3a7254fde081688d72c8c441f45f3a5fff7724
James Young <james@jmsmetaltec.com>
2026-04-09 16:10:36 +0100

    reorder pages

---
+++ pages/4_Machining_Schedule.py
@@ -0,0 +1,5 @@
+# machining changes to weld page:
+# site is done on operation i think? more complex logic
+# different operations - might need their own charts or their own categories or something
+import streamlit as st
+st.title("WORK IN PROGRESS")
