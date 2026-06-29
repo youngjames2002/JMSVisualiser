dce632389ce8a36d14c226231ac7fe98f56969c1
James Young <james@jmsmetaltec.com>
2026-06-29 08:09:35 +0100

    microsoft auth

--- login.py
+++ login.py
@@ -1,48 +1,20 @@
 import streamlit as st
-import streamlit_authenticator as stauth
-from ui_components import *
+from ui_components import render_logo
 
-def to_dict(obj):
-    if hasattr(obj, 'items'):
-        return {k: to_dict(v) for k, v in obj.items()}
-    elif isinstance(obj, list):
-        return [to_dict(i) for i in obj]
-    else:
-        return obj
-    
+
 def require_auth():
-    if not st.session_state.get("authentication_status"):
+    if not st.experimental_user.is_logged_in:
         st.warning("🔒 Please log in first.")
         st.switch_page("login.py")
         st.stop()
-    get_authenticator()
 
-def get_authenticator():
-    """Get authenticator from session or recreate it."""
-    if "authenticator" not in st.session_state:
-        authenticator = stauth.Authenticate(
-            to_dict(st.secrets["credentials"]),
-            st.secrets["cookie"]["name"],
-            st.secrets["cookie"]["key"],
-            st.secrets["cookie"]["expiry_days"],
-        )
-        st.session_state["authenticator"] = authenticator
-    return st.session_state["authenticator"]
 
-authenticator = get_authenticator()
-
-st.session_state["authenticator"] = authenticator
-
-tcol1, tcol2 = st.columns([1,4])
+tcol1, tcol2 = st.columns([1, 4])
 tcol2.title("Data Visualiser - Login")
 render_logo(tcol1)
 
-authenticator.login()
-
-if st.session_state.get("authentication_status"):
+if st.experimental_user.is_logged_in:
     st.success("Login Successful")
     st.write("Use Sidebar to navigate app")
-elif st.session_state.get("authentication_status") is False:
-    st.error("Incorrect username or password")
 else:
-    st.warning("please enter your username and password")
+    st.login("microsoft")
